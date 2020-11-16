8669de4583122af5dda9d9763ba59eb56729eec5
zato91 <zatokey@gmail.com>
2020-11-15 23:18:04 -0600

    frontend folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import { Container } from 'reactstrap'
 
 function App() {
   return (
-    <div className="App">
-     learning
-    </div>
+    <Container >
+        <Login />
+        <Dashboard />
+    </Container>
   );
 }
 
