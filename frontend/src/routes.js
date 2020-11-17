e59b86997f7e335e1dec6b4b8c2c0e7dd918d9da
zato91 <zatokey@gmail.com>
2020-11-16 21:57:17 -0500

    register page

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Login from './pages/Login/'
-
+import Register from './pages/Register'
 import Dashboard from './pages/Dashboard/'
 
 export default function Routes() {
@@ -9,7 +9,7 @@ export default function Routes() {
         <BrowserRouter>
             <Switch>
                 <Route path='/' exact component={Login} />
-          
+                <Route path='/register' exact component={Register} />
                 <Route path='/dashboard' component={Dashboard} />
             </Switch>
         </BrowserRouter>
