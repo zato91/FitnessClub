ee673ad8279f05e84a6f8848a477a6deef556568
zato91 <zatokey@gmail.com>
2020-11-18 23:52:07 -0500

    styling logging

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,10 @@ function App() {
   return (
     <Container>
       <h1>Sport's App</h1>
-      <Routes/>
+      <div className="content">
+        <Routes/>
+
+      </div>
     </Container>
   );
 }
