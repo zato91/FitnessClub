cc7bfaa27169881e8ba49ed43ff77342511cf98c
zato91 <zatokey@gmail.com>
2020-11-30 15:43:16 -0500

    created my registration page

---
+++ frontend/src/pages/MyRegistrations/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Myregistrations(){
+
+    return(
+        <div>
+            My registration component
+        </div>
+    )
+} 
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -5,6 +5,7 @@ import Register from './pages/Register'
 import Dashboard from './pages/Dashboard/'
 import EventsPage from './pages/EventsPage/'
 import TopNav from './components/TopNav'
+import MyRegistration from './pages/MyRegistrations'
 
 export default function Routes() {
     return (
@@ -12,6 +13,7 @@ export default function Routes() {
         <TopNav/>
             <Switch>
                 <Route path='/' exact component={Dashboard} />
+                <Route path='/myregistrations'exact component={MyRegistration}/>
                 <Route path='/login' exact component={Login} />
                 <Route path='/register' exact component={Register} />
                 <Route path="/events" component={EventsPage} />
