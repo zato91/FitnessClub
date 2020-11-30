906e8ebbf981ad9c4f42aaaad796e03a17701ad0
zato91 <zatokey@gmail.com>
2020-11-30 15:47:36 -0500

    Modified myRegistration model

--- backend/src/models/Registration.js
+++ backend/src/models/Registration.js
@@ -1,18 +1,21 @@
 const mongoose = require('mongoose')
 
 const RegistrationSchema = new mongoose.Schema({
-    date: ()=> Date.now(),
-    approved: Boolean,
-   
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
-    },
-    event: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Event"
-    }
+	date: () => Date.now(),
+	approved: Boolean,
+	owner: String,
+	eventTitle: String,
+	eventPrice: String,
+	userEmail:String,
+	eventDate:String,
+	user: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User'
+	},
+	event: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Event'
+	}
 })
 
-module.exports= mongoose.model('Registration',  RegistrationSchema )
-    
+module.exports = mongoose.model('Registration', RegistrationSchema)
