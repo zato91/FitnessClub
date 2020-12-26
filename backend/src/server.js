f0c2d9150a297178b4c870949320e02bba463263
zato91 <zatokey@gmail.com>
2020-12-26 16:06:27 -0500

    created env file

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,7 @@ const path = require('path')
 const http = require('http')
 const socketio = require('socket.io')
 const PORT = process.env.PORT || 8000
+require('dotenv').config()
 
 const app = express()
 const server = http.Server(app)
@@ -22,46 +23,14 @@ io.on('connection', socket => {
 if (process.env.NODE_ENV !== 'production') {
 	require('dotenv').config()
 }
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     next();
-//   });
 
 
 
-
-
-mongoose.connect(`mongodb+srv://user_new:hsgTljJ1owmKUBYh@cluster0.i6xeh.mongodb.net/test?retryWrites=true&w=majority`, {
+mongoose.connect(`mongodb+srv://user_new:6bdwYTuuHaJVdQ2g@cluster0.i6xeh.mongodb.net/test?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
-// try {
-// 	mongoose.connect(process.env.MONGO_DB_SECRET, {
-// 		useNewUrlParser: true,
-// 		useUnifiedTopology: true,
-// 	})
-// 	console.log('MongoDb connected successfully!')
-// } catch (error) {
-// 	console.log(error)
-// }
-// mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.i6xeh.mongodb.net/<dbname>?retryWrites=true&w=majority
-// mongoose.connect(`mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.i6xeh.mongodb.net/test?retryWrites=true&w=majority`,(err)=>{
-// if(err) throw err;
-// console.log("DB Connected Successfully");
-// });
-
-
-// app.use("/files", express.static(path.resolve(__dirname,"..", "files")))
-// app.use(routes);
-
-// server.listen(PORT, () => {
-// 	console.log(`Listening on ${PORT}`)
-// })
-
-
-// SESSION_SECRET=secret 
-// MONGO_DB_CONNECTION=mongodb+srv://new_user01:an0uoUiih5TbURdQ@cluster0.i6xeh.mongodb.net/test?retryWrites=true&w=majority
 
 const connectUsers = {}
 
